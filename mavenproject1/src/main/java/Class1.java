67b161f9afd3574e775902f3a916717b434a26d9
grimlock7786@gmail.com <soblab@Whit209-12.it.farmingdale.edu>
2021-09-08 13:11:34 -0400

    Working on array after populating class

--- mavenproject1/src/main/java/Class1.java
+++ mavenproject1/src/main/java/Class1.java
@@ -10,4 +10,30 @@
  */
 public class Class1 {
     
+    int id;
+    String Name;
+    int[] arr;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+    
+    
+    public static void main(String args[]){
+        
+    }
+    
+    
 }
